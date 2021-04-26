ad747746cff54ee8eb532013b48be8e84ffce4a2
Sourav Chatterjee <souravchatterjee@Souravs-MacBook-Pro-2.local>
2021-04-26 17:57:16 -0400

    Console Log tatements

--- lambdas/git-webhook/webhook/app.js
+++ lambdas/git-webhook/webhook/app.js
@@ -31,7 +31,7 @@ exports.lambdaHandler = async (event, context) => {
   let deployLambdaWebhook = false;
 
   const body = JSON.parse(event.body);
-  console.log("Received Webhook", body);
+  console.log("Received Webhook test", body);
   if (body) {
     const refs = body.ref || null;
     const modified = body.head_commit.modified || [];
