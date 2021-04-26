621b272ee184784173a66adc34ec2d72ed051ab7
Sourav Chatterjee <souravchatterjee@Souravs-MacBook-Pro-2.local>
2021-04-26 12:54:33 -0400

    Webhook Listener

--- lambdas/git-webhook/webhook/app.js
+++ lambdas/git-webhook/webhook/app.js
@@ -18,6 +18,35 @@ exports.lambdaHandler = async (event, context) => {
   console.log("Hello World");
   console.log(event);
   console.log(context);
+
+  const isMainBranchRegex = /\/heads\/main/g;
+  const isUIPipelineReg = /^ui\/.*/g;
+  const isLambdaStopEC2Reg = /^lambdas\/stop-ec2\/.*?/g;
+  const isLambdaStartEC2Reg = /^lambdas\/start-ec2\/.*?/g;
+  const startUIDeloyment = false;
+  const deployLambdaStopEC2 = false;
+  const deployLambdaStartEC2 = false;
+
+  const body = event.body || null;
+  if (body) {
+    const refs = body.ref || null;
+    const modified = body.modified || [];
+    if (refs && isMainBranchRegex.test(refs)) {
+      console.log(
+        "Changes Pushed to Main Branch.. We will match a pipeline..."
+      );
+
+      for (let mod of modified) {
+        if (isUIPipelineReg.test(mod)) startUIDeloyment = true;
+        if (isLambdaStopEC2Reg.test(mod)) deployLambdaStopEC2 = true;
+        if (isLambdaStartEC2Reg.test(mod)) deployLambdaStartEC2 = true;
+      }
+
+      if (startUIDeloyment) console.log("Deploy UI");
+      if (deployLambdaStopEC2) console.log("Deploy Lambda STOP EC2");
+      if (deployLambdaStartEC2) console.log("Deploy Start EC2");
+    }
+  }
   return {
     isBase64Encoded: false,
     statusCode: 200,
